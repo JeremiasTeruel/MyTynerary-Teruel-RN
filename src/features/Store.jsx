ac00ed8a58fb7f8312d5e8bcec34d8945442f0f6
Teje8 <jteruel8@gmail.com>
2022-09-30 13:53:28 -0300

    concat itineraries reducer

--- src/features/Store.jsx
+++ src/features/Store.jsx
@@ -1,14 +1,16 @@
 import { configureStore} from '@reduxjs/toolkit'
 import { setupListeners } from '@reduxjs/toolkit/query'
 import citiesAPI from './CitiesApi'
+import itinerariesAPI from './ItinerariesApi'
 
 const store = configureStore({
 
     reducer:{
         cities : citiesAPI,
         [citiesAPI.reducerPath] : citiesAPI.reducer,
+        [itinerariesAPI.reducerPath] : itinerariesAPI.reducer,
     },
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(citiesAPI.middleware),
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(citiesAPI.middleware, itinerariesAPI.middleware),
 })
 setupListeners(store.dispatch)
 export default store
